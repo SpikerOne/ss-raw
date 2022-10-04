52face3ad5167f8e45245262ac6c0a5e81cefe0c
SpikerOne <96699983+SpikerOne@users.noreply.github.com>
2022-10-05 02:58:29 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,23 +14,7 @@ cd /wwwroot
 tar xvf wwwroot.tar.gz
 rm -rf wwwroot.tar.gz
 
-if [ ! -d /etc/shadowsocks-libev ]; then  
-  mkdir /etc/shadowsocks-libev
-fi
-
-sed -e "/^#/d"\
-    -e "s/\${PASSWORD}/password/g"\
-    -e "s|\${V2_Path}|/v2ray|g"\
-    /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
-echo /etc/shadowsocks-libev/config.json
 cat /etc/shadowsocks-libev/config.json
-
-sed -e "/^#/d"\
-    -e "s/\${PORT}/443/g"\
-    -e "s|\${V2_Path}|/v2ray|g"\
-    -e "$s"\
-    /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf
-echo /etc/nginx/conf.d/ss.conf
 cat /etc/nginx/conf.d/ss.conf
 
 ss-server -c /etc/shadowsocks-libev/config.json &
