a19fcd384d891784ccdf44e73aa4e334e4bcec7c
SpikerOne <96699983+SpikerOne@users.noreply.github.com>
2022-10-05 03:30:08 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 mkdir /v2raybin
 cd /v2raybin
-V2RAY_URL="https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-darwin-amd64-v1.3.2.tar.gz"
+V2RAY_URL="https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-amd64-v1.3.2.tar.gz"
 echo ${V2RAY_URL}
 wget --no-check-certificate ${V2RAY_URL}
-tar -zxvf v2ray-plugin-darwin-amd64-v1.3.2.tar.gz
-rm -rf v2ray-plugin-darwin-amd64-v1.3.2.tar.gz
+tar -zxvf v2ray-plugin-linux-amd64-v1.3.2.tar.gz
+rm -rf v2ray-plugin-linux-amd64-v1.3.2.tar.gz
 mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 rm -rf /v2raybin
 
